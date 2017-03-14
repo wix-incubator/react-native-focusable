14caa506e6f07ca8b4d6c990e87bd08ea924cf70
Daniel Zlotin <danielzl@wix.com>
2017-03-14 17:17:33 +0200

    environment ready

--- src/focusable.test.js
+++ src/focusable.test.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 import renderer from 'react-test-renderer';
 
 describe('focusable', () => {
-  let myScreen, myTextView;
+  let MyScreen;
 
   beforeEach(() => {
-    //
+    MyScreen = require('./MyScreen').default;
   });
 
   it('just works', () => {
-    //
+    const tree = renderer.create(<MyScreen />);
+    expect(tree.toJSON().children).toEqual(['Hello, World!']);
   });
 });
